a4a618f598b323feb52584cc579dbcc2f66584d8
foysauce <mr.foy488@gmail.com>
2024-08-27 21:45:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# matthew-foy-svelte
+ Imma compare svelte with reactJS. Both I am using typescript
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<Button>Click me</Button>
+
+<h1 class="text-4xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
