b418b543342eb1975b0525076dd384c59e02f5c3
foysauce <mr.foy488@gmail.com>
2024-08-31 17:20:58 -0400

    finally got my colors how i want them

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import DarkToggle from '$lib/components/DarkToggle.svelte';
 </script>
+
+<ModeWatcher />
+
+<DarkToggle />
 
 <slot></slot>
