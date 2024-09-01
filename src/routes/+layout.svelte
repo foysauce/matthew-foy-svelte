eee8d9e509ac611b87b6aa123047a2987014857a
foysauce <mr.foy488@gmail.com>
2024-09-01 11:37:14 -0400

    added progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,23 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 	import DarkToggle from '$lib/components/DarkToggle.svelte';
+	import { onMount } from 'svelte';
+
+	let y: number;
+	let windowHeight: number;
+	let fullHeight: number;
+	let scrollableHeight: number;
+	let scrollPercentage: number;
+	$: scrollPercentage = Math.floor((y / scrollableHeight) * 100);
+
+	onMount(() => {
+		fullHeight = document.body.scrollHeight;
+		windowHeight = window.innerHeight;
+		scrollableHeight = fullHeight - windowHeight;
+	});
 </script>
+
+<svelte:window bind:scrollY={y} />
 
 <div
 	class="fixed top-0 z-10 flex w-full flex-row justify-between border-b border-b-primary bg-primary-foreground px-2 py-1"
@@ -16,9 +32,17 @@
 	</div>
 </div>
 
-<div class="fixed bottom-0 right-0 p-3">
-	<h1 class="font-mono">Let &lt;F&gt;: string = "Matthew Foy";</h1>
-	<h1>[##.................................................................]</h1>
+<div class="fixed bottom-0 right-0 p-4">
+	<h1 class="mx-1 font-mono">
+		Let <span class="text-accent">&lt;F&gt;:</span> string =
+		<span class="text-accent">"MATTHEW FOY"</span>
+	</h1>
+	<div class="">
+		[
+		<span>{'#'.repeat(Math.floor(scrollPercentage / 3.25))}</span>
+		<span>{'. '.repeat(35 - Math.floor(scrollPercentage / 2.85))}</span>
+		]
+	</div>
 </div>
 
 <ModeWatcher />
